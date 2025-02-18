073baef343847d65a2fc3f557508fbe62914b5e6
kondo0602 <kondo8363@gmail.com>
2025-02-18 22:54:22 +0900

    refactor: slackへの通知処理をリファクタ

--- src/slack.js
+++ src/slack.js
@@ -4,50 +4,79 @@ const SLACK_WEB_HOOKS_URL = PropertiesService.getScriptProperties().getProperty(
 
 // #admob-revenueにRevenueReportの結果を送信する
 function sendSlackForRevenueReport(revenueReport) {
-	const message = formatReport(revenueReport);
+	const message = formatReportForSlack(revenueReport);
 	Logger.log(message);
 	sendSlackForMessage(message);
 }
 
+// RevenueReportをslackメッセージ用にフォーマットする
+const formatReportForSlack = (revenueReport) => {
+	const appNameColumnWidth = 20;
+	const earningsColumnWidth = 12;
+
+	const summaryLines = [
+		"```",
+		_generateSummaryHeader(appNameColumnWidth, earningsColumnWidth),
+		_generateSeparatorLine(appNameColumnWidth, earningsColumnWidth),
+		..._generateSummaryLine(
+			appNameColumnWidth,
+			earningsColumnWidth,
+			revenueReport,
+		),
+		_generateSeparatorLine(appNameColumnWidth, earningsColumnWidth),
+		_generateTotalLine(
+			appNameColumnWidth,
+			earningsColumnWidth,
+			revenueReport.totalEarnings,
+		),
+		"```",
+	];
+
+	return summaryLines.join("\n");
+};
+
+const _generateSummaryHeader = (appNameColumnWidth, earningsColumnWidth) => {
+	return `| ${"App Name".padEnd(appNameColumnWidth, " ")} | ${"Revenue".padEnd(earningsColumnWidth, " ")} |`;
+};
+
+const _generateSeparatorLine = (width1, width2) => {
+	return `|-${"-".repeat(width1)}-|-${"-".repeat(width2)}-|`;
+};
+
+const _generateSummaryLine = (
+	appNameColumnWidth,
+	earningsColumnWidth,
+	revenueReport,
+) => {
+	return revenueReport.entries.map((entry) => {
+		// NOTE: アプリ名が全角なので半分にする
+		const appName = entry.appName.padEnd(
+			appNameColumnWidth - entry.appName.length / 2,
+			" ",
+		);
+		const earnings = `${entry.earnings.toFixed(0)}円`.padStart(
+			earningsColumnWidth - 1 / 2,
+			" ",
+		);
+		return `| ${appName} | ${earnings} |`;
+	});
+};
+
+const _generateTotalLine = (
+	appNameColumnWidth,
+	earningsColumnWidth,
+	totalEarnings,
+) => {
+	return `| ${"total".padEnd(appNameColumnWidth, " ")} | ${`${totalEarnings.toFixed(0)}円`.padStart(earningsColumnWidth, " ")} |`;
+};
+
 // #admob-revenueにメッセージを送信する
-function sendSlackForMessage(message) {
-	const jsonData = {
-		text: message,
-	};
+const sendSlackForMessage = (message) => {
 	const options = {
 		method: "post",
 		contentType: "application/json",
-		payload: JSON.stringify(jsonData),
+		payload: JSON.stringify({ text: message }),
 	};
+
 	UrlFetchApp.fetch(SLACK_WEB_HOOKS_URL, options);
-}
-
-// RevenueReportをslackメッセージ用にフォーマットする
-function formatReport(revenueReport) {
-	const col1Width = 16; // プラットフォーム列の幅
-	const col2Width = 20; // アプリ名列の幅
-	const col3Width = 12; // 収益列の幅
-
-	const headerPlatform = "Platform".padEnd(col1Width, " ");
-	const headerAppName = "App Name".padEnd(col2Width, " ");
-	const headerEarnings = "Revenue".padEnd(col3Width, " ");
-	let reportSummary = `\`\`\`\n| ${headerPlatform} | ${headerAppName} | ${headerEarnings} |\n`;
-	reportSummary += `|-${"-".repeat(col1Width)}-|-${"-".repeat(col2Width)}-|-${"-".repeat(col3Width)}-|\n`;
-
-	for (const entry of revenueReport.entries) {
-		const platform = entry.platform.padEnd(col1Width, " ");
-		const appName = entry.appName.padEnd(col2Width, " ");
-		const earnings = `${entry.earnings.toFixed(0)}円`.padStart(col3Width, " ");
-		reportSummary += `| ${platform} | ${appName} | ${earnings} |\n`;
-	}
-
-	reportSummary += `|-${"-".repeat(col1Width)}-|-${"-".repeat(col2Width)}-|-${"-".repeat(col3Width)}-|\n`;
-	const totalEarnings = `${revenueReport.totalEarnings.toFixed(0)}円`.padStart(
-		col3Width,
-		" ",
-	);
-	reportSummary += `| ${"total".padEnd(col1Width, " ")} | ${"".padEnd(col2Width, " ")} | ${totalEarnings} |\n`;
-	reportSummary += "```\n";
-
-	return reportSummary;
-}
+};
